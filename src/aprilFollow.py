3b85661fb27b423fb5b10cb81750ba317589450f
Bradley Harris <bradx135@gmail.com>
2024-02-21 13:33:45 +0000

    define camera params in aprilFollow

--- src/aprilFollow.py
+++ src/aprilFollow.py
@@ -79,6 +79,10 @@ def main(baseSpeed, stream, p, i, d):
     robot = Robot()
     driver = Drive(robot, baseSpeed)
     resolution = (480, 360)
+
+    camera_calibration = np.load("calibration/calibration.npy")
+    mtx, dist, rvecs, tvecs = camera_calibration
+    fx, fy, cx, cy = mtx[0, 0], mtx[1, 1], mtx[0, 2], mtx[1, 2]
     
     detector = Detector(families='tag16h5', nthreads=4, quad_decimate=1.0, quad_sigma=0.0,\
                         refine_edges=1, decode_sharpening=0.25, debug=0)
