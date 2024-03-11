485fc765996b32202300fb4b058b6b8670faeefc
Bradley Harris <bradx135@gmail.com>
2024-03-11 07:09:59 -0700

    tuned forward pid

--- src/aprilFollow.py
+++ src/aprilFollow.py
@@ -7,8 +7,10 @@ import numpy as np
 import time
 import argparse
 import cv2
+import os
 import paho.mqtt.publish as publish
 
+bot = os.environ.get('jetbot')
 mqttBroker = "10.0.0.1"
 POLLINTERVAL = 0.01
 
@@ -81,6 +83,8 @@ class Velocity:
         output = p_term + i_term + d_term
         # Update previous error for the next iteration
         self.prev_error = error
+        
+        output= max(0, output)
 
         return output
 
@@ -103,7 +107,7 @@ def main(baseSpeed, tagid, stream, p, i, d):
     # 0.1, 0.0025, 0.2
     # 0.08. 0.015, 0.2
     steering = Steering(0.08, 0.005, 0.2, 0)
-    forward = Velocity(p, i, d, 50)
+    forward = Velocity(0.05, 0, 0, 30)
 
     try:
         while driver.running:
@@ -135,18 +139,22 @@ def main(baseSpeed, tagid, stream, p, i, d):
                 #print(float(center[0]) / float(resolution[0]))
                 
                 hor_pos = 2*(float(center[0]) / float(resolution[0])) - 1.0
+                size = distance(tag.corners[2], tag.corners[3])
                 rot = steering(hor_pos)
-                fwd = forward(distance(tag.corners[2], tag.corners[3]))
-                
+                fwd = forward(size)
+                #print(fwd)
                 #print(f"Rotation: {rot:.2f}")
                 #print(f"forward: {fwd:.2f}, Rotation: {rot:.2f}")
 
                 #if stream:
                     #cv2.putText(image, f"{rot} == {fwd}", (0, 0), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2)
-                
-                print(fwd)
-                driver.forward = 0.2 
+                driver.forward = min(0.2, 0.2*fwd)  
                 driver.steering = rot
+                
+                if size > 30:
+                    steering.reset() 
+                    driver.forward = 0
+                    driver.steering = 0
 
             else:
                 steering.reset()
@@ -155,7 +163,7 @@ def main(baseSpeed, tagid, stream, p, i, d):
                 #print("--- nothing ---")
                 detected = False
 
-            #driver.write()
+            driver.write()
             
             if stream: 
                 cam.stream(image)
@@ -168,7 +176,7 @@ def main(baseSpeed, tagid, stream, p, i, d):
             execution_time = time_taken
             fps = int(1/execution_time)
 
-            #print(f"Left Motor: {robot.left_motor.value:.2f}, Right Motor: {robot.right_motor.value:.2f}, Speed: {driver.speed:.2f}, Loop Speed: {execution_time:.3f} = {int(1/execution_time)}FPS, Detected: {detected}")
+            print(f"Left Motor: {robot.left_motor.value:.2f}, Right Motor: {robot.right_motor.value:.2f}, Speed: {driver.speed:.2f}, Loop Speed: {execution_time:.3f} = {int(1/execution_time)}FPS, Detected: {detected}")
 
     finally:
         robot.stop()
